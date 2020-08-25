48dda5ed9e12738d06e6ea34672cc44582b05416
divisia <omrfyyz@gmail.com>
2020-08-25 19:02:39 +0300

    logout tce rider

--- identity/urls.py
+++ identity/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
 from .views import *
 
 app_name = 'identity'
--- identity/views.py
+++ identity/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.core.exceptions import SuspiciousOperation
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.views.generic import DetailView, ListView
 from django.conf import settings
 
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('content:home')
 
 class RegisterView(View):
     template = 'registration/register.html'
